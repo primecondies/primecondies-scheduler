bbd895b1faa6fa46fd32e2f3981a41e71b8c3211
Trentin Gillis <craishot@gmail.com>
2018-12-23 18:57:39 -0700

    Add Newlin Creek to crags config

--- src/config/crags.js
+++ src/config/crags.js
@@ -1,8 +1,13 @@
 const places = [
   {
     location: "clear creek",
-    "latitude": "39.736792",
-    "longitude": "-105.315946"
+    latitude: "39.736792",
+    longitude: "-105.315946"
+  },
+  {
+    location: "newlin creek",
+    latitude: "38.266752",
+    longitude: "-105.187431"
   }
 ];
 
